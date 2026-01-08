e2f3ece48530edea40b2fea6ee65cbdaee7935a2
Ken Beaudin <30326135+kbventures@users.noreply.github.com>
2026-01-07 20:27:33 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,20 @@
 Install CMDs
+
+Chrome
+
+VS Code
+
+Bash
+
+NVM
+
+Typescript
+
+I3W
+~/.config/i3/config
+
+Obsidian
+sudo snap install obsidian --classic
+
+Anki
+https://docs.ankiweb.net/platform/linux/installing.html
